e9e72b6e1a7761b6f9fb47a2e1d97ef8af179437
JamesWClark <jwclark@rockhursths.edu>
2016-03-09 01:53:18 -0600

    bugs need work

--- keyboard.js
+++ keyboard.js
@@ -5,17 +5,14 @@
  * http://touchtype.co/index.php/typing/lessons/1
  */
 
-var shifted = false;
-var keysDown = {};
+var capsLock = false;
 
 var toggleKeys = function (shift) {
     if (shift) {
-        shifted = true;
         $('.off').hide();
         $('.on').show();
         $('.letter').addClass('uppercase');
     } else {
-        shifted = false;
         $('.on').hide();
         $('.off').show();
         $('.letter').removeClass('uppercase');
@@ -24,8 +21,8 @@ var toggleKeys = function (shift) {
 
 var registerHandlers = function () {
     
-    // Prevent the backspace key from navigating back.
-    //http://stackoverflow.com/questions/1495219/how-can-i-prevent-the-backspace-key-from-navigating-back
+    // prevent backspace (delete) navigation
+    // http://stackoverflow.com/questions/1495219/how-can-i-prevent-the-backspace-key-from-navigating-back
     $(document).unbind('keydown').bind('keydown', function (event) {
         var doPrevent = false;
         if (event.keyCode === 8) {
@@ -51,25 +48,57 @@ var registerHandlers = function () {
         }
     });
 
-
+    // toggle shift
     $(document).on('keyup keydown', function (e) {
         toggleKeys(e.shiftKey);
-        console.log(e.keyCode);
     });
-
+    
+    // key is down
     $(document).on('keydown', function (e) {
-        if(e.keyCode === 191) { // firefox: prevent quick search '/'
-            e.preventDefault();
+        console.log(e.keyCode);
+        switch(e.keyCode) {
+            case 191: // firefox: prevent quick search '/'
+                e.preventDefault();
+                break;
+            case 20: // toggle caps lock
+                capsLock = !capsLock;
+                if(capsLock) {
+                    $('[data-keycode=20]').addClass('keydown');
+                } else {
+                    $('[data-keycode=20]').removeClass('keydown');  
+                }
+                break;
+            default:
+                $('[data-keycode=' + e.keyCode + ']').addClass('keydown');
+                break;
         }
-        keysDown[e.keyCode] = true;
-        $('[data-keycode=' + e.keyCode + ']').addClass('keydown');
     });
 
+    // key is up
     $(document).on('keyup', function (e) {
-        keysDown[e.keyCode] = false;
-        $('[data-keycode=' + e.keyCode + ']').removeClass('keydown');
+        switch(e.keyCode) {
+            case 20: // caps lock
+                // ignore
+                break;
+            default:
+                $('[data-keycode=' + e.keyCode + ']').removeClass('keydown');
+                break;
+        }
     });
 
+    // display keys in text area
+    $(document).on('keypress', function (e) {
+        var s = String.fromCharCode( e.which );
+        var capsKey = $('[data-keycode=20]');
+        if (s.toUpperCase() === s && !e.shiftKey) {
+            capsKey.addClass('keydown');
+        } else {
+            capsLock = false;
+            capsKey.removeClass('keydown');
+        }
+        var txt = $('#write');
+        txt.val(txt.val() + String.fromCharCode(e.keyCode));
+    });
 
 };
 
